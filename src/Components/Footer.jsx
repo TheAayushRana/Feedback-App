5af4d48670e5f8da7f08034218790150b1fe48e7
Aayush Rana <ayushrana38@gmail.com>
2022-10-23 22:39:10 +0530

    Footer Updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { FaHeart } from "react-icons/fa";
 
 export default function Footer() {
-  return <Footer>Made with Love By Aayush Rana</Footer>;
+  return (
+    <div className="footer">
+      <h3>
+        Made with <FaHeart /> By Aayush Rana
+      </h3>
+    </div>
+  );
 }
